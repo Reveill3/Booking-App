f50e17b752fcf20632969fae812d6274e848ecc6
Reveill3 <38196983+Reveill3@users.noreply.github.com>
2022-11-23 11:02:09 -0600

    hours: 1 - base pages/components themeing

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,39 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Cars from './pages/Cars';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Reservation from './pages/Reservation';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#37474f',
+      light: '#62727b',
+      dark: '#102027',
+    },
+    secondary: {
+      main: '#f5f5f5',
+      light: '#ffffff',
+      dark: '#c2c2c2',
+    },
+  },
+});
+
 function App() {
   return (
-    <div>
-      Hello world!
-    </div>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/cars' element={<Cars />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/reservation' element={<Reservation />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Header from '../components/Header';
+import NavBar from '../components/NavBar';
+
+const Home = () => {
+  return (
+    <div>
+      <NavBar />
+      <Header />
+    </div>
+  );
+};
+
+export default Home;
