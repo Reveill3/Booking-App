b54f992fd1488ec63e03e739a10ac01e99bff9b0
Reveill3 <38196983+Reveill3@users.noreply.github.com>
2022-11-23 13:14:17 -0600

    11.23.22:2 Finish - Navbar Header work

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,16 @@
+import { Box } from '@mui/material';
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <Box>
+      <video
+        src='https://res.cloudinary.com/ddq3k3ntz/video/upload/v1669230118/Pexels_Videos_1437396_zafgx1.mp4'
+        autoPlay
+        loop
+        muted
+      />
+    </Box>
+  );
 };
 
 export default Header;
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,5 +1,36 @@
+import styled from '@emotion/styled';
+import { Button, Toolbar, Typography } from '@mui/material';
+import AppBar from '@mui/material/AppBar';
+import ButtonGroup from '@mui/material/ButtonGroup';
+
+const StyledToolbar = styled(Toolbar)({
+  display: 'flex',
+  justifyContent: 'space-between',
+});
+
+const SpacedButton = styled(Button)({
+  marginRight: '10px',
+});
+
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <div>
+      <AppBar position='static'>
+        <StyledToolbar>
+          <img
+            src='https://res.cloudinary.com/ddq3k3ntz/image/upload/v1669229163/White_logo_-_no_background_oecant.png'
+            alt='logo'
+            height={100}
+            width={200}
+          />
+          <ButtonGroup variant='contained' color='secondary'>
+            <SpacedButton>Login</SpacedButton>
+            <SpacedButton>Register</SpacedButton>
+          </ButtonGroup>
+        </StyledToolbar>
+      </AppBar>
+    </div>
+  );
 };
 
 export default NavBar;
