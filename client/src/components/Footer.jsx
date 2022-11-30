08cb6b59e1078df0c19903573ea332f451e4dfe0
Reveill3 <38196983+Reveill3@users.noreply.github.com>
2022-11-30 17:20:25 -0600

    footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,10 +1,19 @@
 import { useTheme } from '@emotion/react';
 import { Box } from '@mui/material';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import IconButton from '@mui/material/IconButton';
+import Stack from '@mui/material/Stack';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+
+const admin = {
+  instagramurl: 'https://www.instagram.com/revrentalsdfw',
+  facebookurl: 'https://www.facebook.com/revrentals',
+};
 
 const Footer = () => {
   const theme = useTheme();
-
+  const navigate = useNavigate();
   return (
     <Box
       sx={{
@@ -14,12 +23,26 @@ const Footer = () => {
         display: 'flex',
         justifyContent: 'space-between',
         alignItems: 'center',
+        padding: { sm: '0 40px', xs: '0 20px' },
       }}
     >
-      <Link to='/privacy'>Privacy Policy</Link>
-      <Link to='/privacy'>Privacy Policy</Link>
-      <Link to='/privacy'>Privacy Policy</Link>
-      <Link to='/privacy'>Privacy Policy</Link>
+      <Link to='/privacy' style={{ color: 'white' }}>
+        Privacy Policy
+      </Link>
+      <Link to='/privacy' style={{ color: 'white' }}>
+        Contact Us
+      </Link>
+      <Stack direction='row'>
+        <IconButton size='large' onClick={() => window.open(admin.facebookurl)}>
+          <FacebookIcon sx={{ color: 'white' }} fontSize='large' />
+        </IconButton>
+        <IconButton
+          size='large'
+          onClick={() => window.open(admin.instagramurl)}
+        >
+          <InstagramIcon sx={{ color: 'white' }} fontSize='large' />
+        </IconButton>
+      </Stack>
     </Box>
   );
 };
